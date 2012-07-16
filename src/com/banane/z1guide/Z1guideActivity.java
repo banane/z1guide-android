1d2daa398fd5c6a934efc6f2871528efd3ad3b32
Anna Billstrom <banane@gmail.com>
2012-07-16 16:55:22 -0700

    first commit

---
+++ src/com/banane/z1guide/Z1guideActivity.java
@@ -0,0 +1,13 @@
+package com.banane.z1guide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Z1guideActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
