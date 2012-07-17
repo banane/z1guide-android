6c1823a88a8bcdc70660bf9b7497aa818dbdfaa8
Anna Billstrom <banane@gmail.com>
2012-07-16 17:30:30 -0700

    first page

--- src/com/banane/z1guide/Z1guideActivity.java
+++ src/com/banane/z1guide/Z1guideActivity.java
@@ -2,6 +2,7 @@ package com.banane.z1guide;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Z1guideActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,17 @@ public class Z1guideActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public void viewAbout(View v){
+    	
+    }
+    
+    public void viewLocations(View v){
+    	
+    }
+    
+    public void viewPrograms(View v){
+    	
     }
 }
